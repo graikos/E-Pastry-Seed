46515252d3c29cda5010221bdd1aac5e4415de0e
graikos <raikosgiorgos7@gmail.com>
2021-12-24 20:30:42 +0200

    Separated seed and add_node requests

--- handlers.py
+++ handlers.py
@@ -3,30 +3,28 @@ from utils import log
 from random import choice, randint
 
 REQUEST_MAP = {
-    "add_node": lambda event_queue, body, server: add_node(event_queue, body, server),
+    "get_seed": lambda event_queue, body, server: get_seed(body, server),
+    "add_node": lambda event_queue, body, server: add_node(event_queue, body),
     "dead_node": lambda event_queue, body, server: dead_node(event_queue, body)
 }
 
 STATUS_OK = 200
 STATUS_NOT_FOUND = 404
 
-def add_node(event_queue, body, server):
+def get_seed(body, server):
     """
-    Reads node IP and PORT from request body and puts it in event queue to be added in the main thread
-    Returns response, consisting of IP and PORT of random seed choice
-    :param event_queue: shared queue
+    Returns IP and PORT of random node
     :param body: the request body
     :param server: the Server object
-    :return: None
+    :return: response containing IP and PORT of seed
     """
-
     random_seed_choice = None
     conn_addr = body["ip"], body["port"]
     if conn_addr in server.nodes:
         if len(server.nodes) > 1:
             random_seed_index = randint(0, len(server.nodes) - 1)
             if server.nodes[random_seed_index] == conn_addr:
-                random_seed_choice = server.nodes[(random_seed_index + 1)%len(server.nodes)]
+                random_seed_choice = server.nodes[(random_seed_index + 1) % len(server.nodes)]
             else:
                 random_seed_choice = server.nodes[random_seed_index]
     else:
@@ -36,24 +34,36 @@ def add_node(event_queue, body, server):
             pass
 
     log.info(f"Got request for seed, sending {random_seed_choice}")
-    event_queue.put((body["ip"], body["port"], 1))
+
     if random_seed_choice is None:
         status = STATUS_NOT_FOUND
-        body = {}
+        resp_body = {}
     else:
         status = STATUS_OK
-        body = {"ip": random_seed_choice[0], "port": random_seed_choice[1]}
+        resp_body = {"ip": random_seed_choice[0], "port": random_seed_choice[1]}
 
-    header = {"status": status, "type": "seed_node"}
+    resp_header = {"status": status, "type": "seed_node"}
+    return utils.create_request(resp_header, resp_body)
 
-    return utils.create_request(header, body)
+
+def add_node(event_queue, body):
+    """
+    Reads node IP and PORT from request body and puts it in event queue to be added in the main thread
+    :param event_queue: shared queue
+    :param body: the request body
+    :return: response with OK status
+    """
+    event_queue.put((body["ip"], body["port"], 1))
+
+    return utils.create_request({"type": "add", "status": STATUS_OK}, {})
+
 
 def dead_node(event_queue, body):
     """
     Adds node whose ID and PORT are contained in request body to event queue, to be removed by the main thread
     :param event_queue: shared queue
     :param body: the request body
-    :return: None
+    :return: response with OK status
     """
     event_queue.put((body["ip"], body["port"], 0))
 
